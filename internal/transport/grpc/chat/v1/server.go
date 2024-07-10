30b7357e1b4403745cec693d6fbcfee4a09d9c9f
Andrei Dubov <av.dubov@yandex.ru>
2024-07-10 11:08:20 +0400

    removed empty line in SendMessage function

--- internal/transport/grpc/chat/v1/server.go
+++ internal/transport/grpc/chat/v1/server.go
@@ -28,7 +28,6 @@ func (c *chatServer) Create(ctx context.Context, r *chat_v1.CreateRequest) (*cha
 }
 
 func (c *chatServer) SendMessage(ctx context.Context, r *chat_v1.SendMessageRequest) (*empty.Empty, error) {
-
 	const op = "ChatServer.SendMessageRequest"
 
 	log.Printf("%s: input = %+v", op, r)
