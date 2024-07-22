cfd8d9d5b8d21257b2f38117574f750aca3101b0
Andrei Dubov <av.dubov@yandex.ru>
2024-07-22 14:40:21 +0400

    fixed chat repository interface

--- internal/repository/repository.go
+++ internal/repository/repository.go
@@ -8,5 +8,5 @@ import (
 type Chats interface {
 	Create(ctx context.Context, name string, userIDs []int64) (int64, error)
 	CreateMessage(ctx context.Context, userID, chatID int64, message string) (int64, int64, error)
-	Delete(ctx context.Context, chatID int64) error
+	Delete(ctx context.Context, chatID int64) (int64, error)
 }
