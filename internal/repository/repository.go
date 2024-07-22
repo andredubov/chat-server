71f1b3c64a7c391d1b965af38c2a4bd712f8156a
Andrei Dubov <av.dubov@yandex.ru>
2024-07-22 15:08:52 +0400

    added chat repository interface

--- internal/repository/repository.go
+++ internal/repository/repository.go
@@ -7,6 +7,11 @@ import (
 // Chats provide inteface for chats repository
 type Chats interface {
 	Create(ctx context.Context, name string, userIDs []int64) (int64, error)
-	CreateMessage(ctx context.Context, userID, chatID int64, message string) (int64, int64, error)
 	Delete(ctx context.Context, chatID int64) (int64, error)
+}
+
+// Messages provide inteface for message repository
+type Messages interface {
+	Create(ctx context.Context, chatID, userID int64, message string) (int64, error)
+	Delete(ctx context.Context, messageID int64) (int64, error)
 }
