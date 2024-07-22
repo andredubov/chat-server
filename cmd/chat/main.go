e93f71f6da3578027c03245914b43e5b296ade70
Andrei Dubov <av.dubov@yandex.ru>
2024-07-22 10:36:20 +0400

    added grpc config to the chat-server

--- cmd/chat/main.go
+++ cmd/chat/main.go
@@ -1,20 +1,29 @@
 package main
 
 import (
-	"fmt"
 	"log"
 	"net"
 
+	"github.com/andredubov/chat-server/internal/config"
+	"github.com/andredubov/chat-server/internal/config/env"
 	server "github.com/andredubov/chat-server/internal/transport/grpc/chat/v1"
 	chat_v1 "github.com/andredubov/chat-server/pkg/chat/v1"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/reflection"
 )
 
-const grpcPort = 50052
-
 func main() {
-	listen, err := net.Listen("tcp", fmt.Sprintf(":%d", grpcPort))
+	err := config.Load()
+	if err != nil {
+		log.Fatalf("failed to load config: %v", err)
+	}
+
+	grpcConfig, err := env.NewGRPCConfig()
+	if err != nil {
+		log.Fatalf("failed to get grpc config: %v", err)
+	}
+
+	listen, err := net.Listen("tcp", grpcConfig.Address())
 	if err != nil {
 		log.Fatalf("failed to listen: %v", err)
 	}
