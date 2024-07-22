3c678f399163e1ee53f1b0f5ee4aaf6c989916a4
Andrei Dubov <av.dubov@yandex.ru>
2024-07-22 12:03:00 +0400

    added chat repository package

---
+++ internal/repository/repository.go
@@ -0,0 +1,12 @@
+package repository
+
+import (
+	"context"
+)
+
+// Chats provide inteface for chats repository
+type Chats interface {
+	Create(ctx context.Context, name string, userIDs []int64) (int64, error)
+	CreateMessage(ctx context.Context, userID, chatID int64, message string) (int64, int64, error)
+	Delete(ctx context.Context, chatID int64) error
+}
