75c0fef5e0f5b05bc3c04595dc836ae911e3338e
Andrei Dubov <av.dubov@yandex.ru>
2024-07-22 15:32:53 +0400

    added config load into the entry point application

--- cmd/chat/main.go
+++ cmd/chat/main.go
@@ -6,8 +6,10 @@ import (
 
 	"github.com/andredubov/chat-server/internal/config"
 	"github.com/andredubov/chat-server/internal/config/env"
+	"github.com/andredubov/chat-server/internal/repository/postgres"
 	server "github.com/andredubov/chat-server/internal/transport/grpc/chat/v1"
 	chat_v1 "github.com/andredubov/chat-server/pkg/chat/v1"
+	"github.com/andredubov/chat-server/pkg/database"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/reflection"
 )
@@ -23,6 +25,20 @@ func main() {
 		log.Fatalf("failed to get grpc config: %v", err)
 	}
 
+	postgresConfig, err := env.NewPostgresConfig()
+	if err != nil {
+		log.Fatalf("failed to get postgres config: %v", err)
+	}
+
+	connection, err := database.NewPostgresConnection(postgresConfig)
+	if err != nil {
+		log.Fatalf("failed to get postgres connection: %v", err)
+	}
+	defer connection.Close()
+
+	chatsRepository := postgres.NewChatsRepository(connection)
+	messagesRepository := postgres.NewMessagesRepository(connection)
+
 	listen, err := net.Listen("tcp", grpcConfig.Address())
 	if err != nil {
 		log.Fatalf("failed to listen: %v", err)
@@ -30,7 +46,7 @@ func main() {
 
 	s := grpc.NewServer()
 	reflection.Register(s)
-	chat_v1.RegisterChatServer(s, server.NewChatServer())
+	chat_v1.RegisterChatServer(s, server.NewChatServer(chatsRepository, messagesRepository))
 
 	log.Printf("server listening at %v", listen.Addr())
 
