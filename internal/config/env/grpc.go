d337d13d0a29cfee69167fcb771aa81bdc4838eb
Andrei Dubov <av.dubov@yandex.ru>
2024-07-25 15:24:43 +0400

    fixed env grpc config

--- internal/config/env/grpc.go
+++ internal/config/env/grpc.go
@@ -20,16 +20,14 @@ type grpcConfig struct {
 
 // NewGRPCConfig returns an instance of grpcConfig struct
 func NewGRPCConfig() (config.GRPCConfig, error) {
-	const op = "env.NewGRPCConfig"
-
 	host := os.Getenv(grpcHostEnvName)
 	if len(host) == 0 {
-		return nil, fmt.Errorf("%s: %s", op, "grpc host not found")
+		return nil, fmt.Errorf("%s", "grpc host not found")
 	}
 
 	port := os.Getenv(grpcPortEnvName)
 	if len(port) == 0 {
-		return nil, fmt.Errorf("%s: %s", op, "grpc port not found")
+		return nil, fmt.Errorf("%s", "grpc port not found")
 	}
 
 	return &grpcConfig{
