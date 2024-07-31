79566a84b599dd40d789057a1c9af284cea9c6c1
Andrei Dubov <av.dubov@yandex.ru>
2024-07-31 20:36:06 +0400

    added create method for the chat service

---
+++ internal/service/chat/create.go
@@ -0,0 +1,33 @@
+package chat
+
+import (
+	"context"
+
+	"github.com/andredubov/chat-server/internal/service/model"
+)
+
+func (c *chatsService) Create(ctx context.Context, chat model.Chat) (int64, error) {
+	var chatID int64
+	err := c.txManager.ReadCommitted(ctx, func(ctx context.Context) (errTx error) {
+		chatID, errTx = c.chatsRepository.Create(ctx, chat)
+		if errTx != nil {
+			return errTx
+		}
+
+		for _, userID := range chat.UserIDs {
+			participant := model.Participant{
+				ChatID: chat.ID,
+				UserID: userID,
+			}
+			c.participantsRepository.Create(ctx, participant)
+		}
+
+		return nil
+	})
+
+	if err != nil {
+		return 0, err
+	}
+
+	return chatID, nil
+}
