8a86e7e305f71dc48bcbf9d19e8eb26171cd5ee1
Andrei Dubov <av.dubov@yandex.ru>
2024-07-31 18:40:12 +0400

    added messages repository layer

---
+++ internal/repository/postgres/message/create.go
@@ -0,0 +1,35 @@
+package postgres
+
+import (
+	"context"
+
+	sq "github.com/Masterminds/squirrel"
+	"github.com/andredubov/chat-server/internal/client/database"
+	"github.com/andredubov/chat-server/internal/service/model"
+)
+
+// Create a new message in the message repository
+func (m *messagesRepository) Create(ctx context.Context, message model.Message) (int64, error) {
+	builderInsert := sq.Insert(messagesTable).
+		PlaceholderFormat(sq.Dollar).
+		Columns(chatIDMessagesTableColumn, userIDMessagesTableColumn, messageMessagesTableColumn).
+		Values(message.ToChatID, message.FromUserID, message.Text).
+		Suffix("RETURNING id")
+
+	query, args, err := builderInsert.ToSql()
+	if err != nil {
+		return 0, err
+	}
+
+	q, messageID := database.Query{
+		Name:     "messagesRepository.Create",
+		QueryRaw: query,
+	}, int64(0)
+
+	err = m.dbClient.Database().QueryRowContext(ctx, q, args...).Scan(&messageID)
+	if err != nil {
+		return 0, err
+	}
+
+	return messageID, nil
+}
